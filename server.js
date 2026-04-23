32d1699893163694d80d118b656fbcd1d3722a37
choity <choitybarai2234@gmail.com>
2026-04-23 21:51:53 +0600

    fix railway issue

--- server.js
+++ server.js
@@ -291,3 +291,6 @@ app.get('/register', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // ─── START ─────────────────────────────────────────────────────
+app.get("/test", (req, res) => {
+    res.send("Server OK 🚀");
+});
